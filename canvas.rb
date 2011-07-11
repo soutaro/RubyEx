5cd3ebd3c78131622fa2525c8928f416446c9292
Soutaro Matsumoto (felix) <matsumoto@soutaro.com>
2011-07-11 11:32:13 +0900

    Canvas with fill color

--- canvas.rb
+++ canvas.rb
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: emacs-mule -*-
 require 'cairo'
 
 class Canvas
@@ -13,7 +13,7 @@ class Canvas
     @surface = Cairo::ImageSurface.new(format, width, height)
     @context = Cairo::Context.new(surface)
     
-    @context.set_source_rgb(1, 1, 1) # ç™½
+    @context.set_source_rgb(1, 1, 1) # ’Çò
     @context.rectangle(0, 0, width, height)
     @context.fill
     
@@ -28,24 +28,45 @@ class Canvas
     this.surface.write_to_png(file)
   end
 
-  def line(x1,y1,x2,y2)
+  def line(x1,y1,x2,y2,options = {})
+    options[:stroke] = [0,0,0,1] unless options[:stroke]
+    
     context.move_to(x1,y1)
     context.line_to(x2,y2)
+    
+    context.set_source_rgba(*options[:stroke])
     context.stroke
   end
 
-  def square(x,y,size)
+  def square(x,y,size, options = {})
+    options[:stroke] = [0,0,0,1] unless options[:stroke]
+    options[:fill] = [0,0,0,0] unless options[:fill]
+    
+    context.set_source_rgba(*options[:stroke])
     context.rectangle(x,y,size,size)
     context.stroke
+    
+    context.set_source_rgba(*options[:fill])
+    context.rectangle(x,y,size,size)
+    context.fill
   end
 
-  def circle(x,y,r)
+  def circle(x,y,r, options = {})
+    options[:stroke] = [0,0,0,1] unless options[:stroke]
+    options[:fill] = [0,0,0,0] unless options[:fill]
+    
+    context.set_source_rgba(*options[:stroke])
     context.arc(x, y, r, 0, 2 * Math::PI)
     context.stroke
+
+    context.set_source_rgba(*options[:fill])
+    context.arc(x, y, r, 0, 2 * Math::PI)
+    context.fill
   end
 end
 
-# Canvas.create("hello.png") do |g|
-#   g.line(0,0,200,300)
-#   g.square(200,300,30)
-# end
+Canvas.create("hello.png") do |g|
+  g.line(0,0,200,300, :stroke => [0,1,0,1])
+  g.square(200,300,30, :stroke => [0,1,1,1], :fill => [1,0,0,0.5])
+  g.circle(100,100,100, :fill => [0,0,1,0.3])
+end
