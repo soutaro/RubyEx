43e711de681660e9f08c47c84a357e38d012654b
Soutaro Matsumoto (felix) <matsumoto@soutaro.com>
2011-07-11 11:32:26 +0900

    Hash with default block used

--- animal_2.rb
+++ animal_2.rb
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
-animals = {}
+
+animals = Hash.new() do |hash, key|
+  while true
+    puts("#{key}は知らない動物です。登録しますか？(Y/N)")
+    yn = gets.chomp
+    
+    if yn == "y" || yn == "Y"
+      puts("#{key}の鳴き声を入力してください")
+      value = gets.chomp
+      value = nil if value == ""
+      animals[key] = value
+      break
+    elsif yn == "n" || yn == "N"
+      break
+    end
+  end
+  
+  value
+end
 
 animals["犬"] = "わんわん"
 animals["猫"] = "ニャーニャー"
@@ -9,30 +27,16 @@ animals["蛇"] = nil
 
 while true
   puts("動物を漢字で指定してください（中止は空打ち）")
+  
   key = gets.chomp
   if key == ""
     break
   end
+  
   value = animals[key]
-  if animals.has_key?(key)
-    unless value
-      puts("#{key}は鳴きません")
-    else
-      puts("#{key}の鳴き声は#{value}です")
-    end
+  unless value
+    puts("#{key}は鳴きません")
   else
-    while true
-      puts("#{key}は知らない動物です。登録しますか？(Y/N)")
-      yn = gets.chomp
-      if yn == "y" || yn == "Y"
-        puts("#{key}の鳴き声を入力してください")
-        value = gets.chomp
-        value = nil if value == ""
-        animals[key] = value
-        break
-      elsif yn == "n" || yn == "N"
-        break
-      end
-    end
+    puts("#{key}の鳴き声は#{value}です")
   end
 end
